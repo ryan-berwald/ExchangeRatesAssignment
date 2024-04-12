5bb3e1816d0fc4e6ec577cd49636dca297607203
Ryan Berwald <rberwald@gmail.com>
2024-04-12 01:02:07 -0500

    Added exchange create factory to create proper data

---
+++ ExchangeRatesAssignment.Api/Factories/ExchangeRateFactory.cs
@@ -0,0 +1,26 @@
+﻿using ExchangeRatesAssignment.Api.Domain;
+using ExchangeRatesAssignment.Api.Utils;
+
+namespace ExchangeRatesAssignment.Api.Factories;
+
+public class ExchangeRateFactory
+{
+    public PartnerRate BuildLatestPartnerRatesModel(List<PartnerRate> partnerRates, string countryCode)
+    {
+        var country = CountryConfig.GetCountries()[countryCode];
+
+        var latestRate = partnerRates.Select(e => e).Where(e => e.Currency.ToUpper() == country.CurrencyCode.ToUpper())
+            .OrderByDescending(e => e.AcquiredDate).First();
+        
+        latestRate.Rate = AdjustRate(latestRate.Rate, country);
+        
+        return latestRate;
+    }
+
+    private decimal AdjustRate(decimal latestRate, Country country)
+    {
+        latestRate += country.flatRate;
+        return Math.Round(latestRate, 2);
+    }
+
+}
