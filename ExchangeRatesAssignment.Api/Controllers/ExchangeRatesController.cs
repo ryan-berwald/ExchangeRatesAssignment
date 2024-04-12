d51980b5ef9ac6293665b2bb746d96e37e085d05
Ryan Berwald <rberwald@gmail.com>
2024-04-12 01:02:52 -0500

    Made proper adjustments to controller

--- ExchangeRatesAssignment.Api/Controllers/ExchangeRatesController.cs
+++ ExchangeRatesAssignment.Api/Controllers/ExchangeRatesController.cs
@@ -1,12 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
 using ExchangeRatesAssignment.Api.Contracts;
+using ExchangeRatesAssignment.Api.Interfaces;
 
 namespace ExchangeRatesAssignment.Api.Controllers
 {
     [ApiController]
-    public class ExchangeRatesController : ControllerBase
+    [Route("api/[controller]")]
+    public class ExchangeRatesController(IExchangeRateService exchangeRateService, IValidationService validationService) : ControllerBase
     {
-        // TODO : Implement an API endpoint that takes a three letter country code and returns a collection of
-        // calculated exchange rates of the type Api.Contracts.PangeaExchangeRate
+        [HttpGet]
+        public IActionResult Get(
+            [FromQuery] string countryCode)
+        {
+            var errors = validationService.ValidateCountryCode(countryCode.ToUpper());
+            if (errors.Count > 0)
+            {
+                return BadRequest(errors.Select(e => $"{e}"));
+            }
+            
+            return Ok(exchangeRateService.ConvertExchangeRate(countryCode.ToUpper()));
+        }
     }
 }
