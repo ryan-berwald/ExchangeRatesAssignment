ab0ae422118452d37658d5c033eaec8d3d49c0bc
Ryan Berwald <rberwald@gmail.com>
2024-04-12 01:00:40 -0500

    Added unit tests

---
+++ ExchangeRatesAssignment.UnitTests/ExchangeRateServiceTests.cs
@@ -0,0 +1,23 @@
+﻿using ExchangeRatesAssignment.Api.Interfaces;
+using ExchangeRatesAssignment.Api.Services;
+using ExchangeRatesAssignment.Repositories;
+
+namespace ExchangeRatesAssignment.UnitTests;
+
+public class ExchangeRateServiceTests
+{
+    private static readonly IPartnerRatesRepository _partnerRatesRepository = new PartnerRatesRepository();
+    private static readonly IExchangeRateService _exchangeRateService = new ExchangeRateService(_partnerRatesRepository);
+
+    
+    [Theory]
+    [InlineData("GTM", "GTQ")]
+    [InlineData("IND", "INR")]
+    [InlineData("MEX", "MXN")]
+    [InlineData("PHL", "PHP")]
+    public void GetDataFromFileSuccess(string countryCode, string expectedCurrency)
+    {
+        var partnerRate = _exchangeRateService.ConvertExchangeRate(countryCode);
+        Assert.Equal(partnerRate.Currency, expectedCurrency);
+    }
+}
---
+++ ExchangeRatesAssignment.UnitTests/ValidationServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using ExchangeRatesAssignment.Api.Interfaces;
+using ExchangeRatesAssignment.Api.Services;
+
+namespace ExchangeRatesAssignment.UnitTests;
+
+public class ValidationServiceTests
+{
+    private static readonly IValidationService _validationService = new ValidationService();
+
+    [Fact]
+    public void ValidCountryCodeReceived()
+    {
+        var countryCode = "MEX";
+        var errors = _validationService.ValidateCountryCode(countryCode);
+        Assert.Empty(errors);
+    }
+
+    [Fact]
+    public void InvalidCountryCodeAndInvalidLengthReceived()
+    {
+        var countryCode = "m";
+        var errors = _validationService.ValidateCountryCode(countryCode);
+        Assert.Equal(2, errors.Count);
+        Assert.Contains("Country code must be 3 characters.", errors);
+        Assert.Contains("Country code not supported.", errors);
+    }
+        
+    [Fact]
+    public void InvalidCountryCodeReceived()
+    {
+        var countryCode = "ABC";
+        var errors = _validationService.ValidateCountryCode(countryCode);
+        Assert.Single(errors);
+        Assert.Contains("Country code not supported.", errors);
+    }
+}
