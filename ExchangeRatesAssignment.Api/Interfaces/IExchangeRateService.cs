58d1dc7c7ebf1379c3a53bf5e3e393c14e4859bf
Ryan Berwald <rberwald@gmail.com>
2024-04-12 01:00:04 -0500

    Added exchange rate service

---
+++ ExchangeRatesAssignment.Api/Interfaces/IExchangeRateService.cs
@@ -0,0 +1,8 @@
+﻿using ExchangeRatesAssignment.Api.Domain;
+
+namespace ExchangeRatesAssignment.Api.Interfaces;
+
+public interface IExchangeRateService
+{
+    public PartnerRate ConvertExchangeRate(string countryCode);
+}
---
+++ ExchangeRatesAssignment.Api/Services/ExchangeRateService.cs
@@ -0,0 +1,17 @@
+﻿using ExchangeRatesAssignment.Api.Domain;
+using ExchangeRatesAssignment.Api.Factories;
+using ExchangeRatesAssignment.Api.Interfaces;
+using ExchangeRatesAssignment.Repositories;
+
+namespace ExchangeRatesAssignment.Api.Services;
+
+public class ExchangeRateService(
+    IPartnerRatesRepository partnerRatesRepository) : IExchangeRateService
+{
+    public PartnerRate ConvertExchangeRate(string countryCode)
+    {
+        var exchangeRateFactory = new ExchangeRateFactory();
+        var partnerRates = partnerRatesRepository.GetPartnerRatesAsync(countryCode).Result.ToList();
+        return exchangeRateFactory.BuildLatestPartnerRatesModel(partnerRates, countryCode);
+    }
+}
