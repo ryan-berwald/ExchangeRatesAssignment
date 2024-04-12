42ed55a0af36ea49a182caf9a5eb56566b81283c
Ryan Berwald <rberwald@gmail.com>
2024-04-12 00:59:17 -0500

    Added Validation Service

---
+++ ExchangeRatesAssignment.Api/Interfaces/IValidationService.cs
@@ -0,0 +1,6 @@
+﻿namespace ExchangeRatesAssignment.Api.Interfaces;
+
+public interface IValidationService
+{
+    public List<string> ValidateCountryCode(string CountryCode);
+}
---
+++ ExchangeRatesAssignment.Api/Services/ValidationService.cs
@@ -0,0 +1,24 @@
+﻿using ExchangeRatesAssignment.Api.Interfaces;
+using ExchangeRatesAssignment.Api.Utils;
+
+namespace ExchangeRatesAssignment.Api.Services;
+
+public class ValidationService: IValidationService
+{
+    public List<string>? ValidateCountryCode(string countryCode)
+    {
+        var countries = CountryConfig.GetCountries();
+        var errors = new List<string>();
+        if (countryCode.Length != 3)
+        {
+            errors.Add("Country code must be 3 characters.");
+        }
+        
+        if (!countries.ContainsKey(countryCode))
+        {
+            errors.Add("Country code not supported.");
+        }
+
+        return errors;
+    }
+}
